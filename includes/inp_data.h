25e3c06c5254ea496a6a087a4a355e6a81df93bd
Deniz Caglar Cingoz <dcingoz@c3r8p2.42vienna.com>
2024-08-15 20:13:01 +0200

    new input struct

---
+++ includes/inp_data.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   inp_data.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcingoz <dcingoz@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/15 20:08:33 by dcingoz           #+#    #+#             */
+/*   Updated: 2024/08/15 20:11:36 by dcingoz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INP_DATA_H
+# define INP_DATA_H
+
+# include "libraries.h"
+
+typedef struct s_inp_data
+{
+	char	*no_path;
+	char	*so_path;
+	char	*we_path;
+	char	*ea_path;
+	char	*sprite_path;
+	int		floor_color;
+	int		ceiling_color;
+	char	**map;
+	int		res_y;
+	int		res_x;
+} t_inp_data;
+
+#endif
